78a9e6f9b1cb40ab13e0bc6df4d5280ea09d36ef
LoganTackett1 <logantackettwork@gmail.com>
2023-09-15 13:50:38 -0700

    fixed incorrect object method formating created receive hit created place ship created dirvect function created slotcheck function as precheck for placeship

--- src/board.js
+++ src/board.js
@@ -1,6 +1,40 @@
+import Vector from "./vectors";
+
+function newShip(length) {
+  const ship = {
+    hits: 0,
+    length,
+    dead: false,
+  };
+  ship.hit = function () {
+    this.hits += 1;
+    if (this.hits === this.length) {
+      this.dead = true;
+    }
+  };
+  return ship;
+}
+
+function dirVect(dir) {
+  if (dir === "right") {
+    return new Vector(0, 1);
+  }
+  if (dir === "left") {
+    return new Vector(0, -1);
+  }
+  if (dir === "up") {
+    return new Vector(-1, 0);
+  }
+  if (dir === "down") {
+    return new Vector(1, 0);
+  }
+  throw Error("Invalid direction in dirVect");
+}
+
 export function newBoard() {
   const board = {};
 
+  board.ships = [];
   board.area = [];
   for (let i = 0; i < 10; i += 1) {
     board.area.push([]);
@@ -8,11 +42,49 @@ export function newBoard() {
   board.area.forEach((row) => {
     for (let i = 0; i < 10; i += 1) {
       row.push({
-        ship: false,
+        ship: null,
         hit: false,
       });
     }
   });
+
+  board.checkSlots = function (row, col, length, dir) {
+    const vect = dirVect(dir);
+    const init = new Vector(row, col);
+    for (let i = 0; i < length; i += 1) {
+      if (!this.area[init.x0][init.x1]) {
+        return false;
+      }
+      if (this.area[init.x0][init.x1].ship) {
+        return false;
+      }
+      init.add(vect);
+    }
+    return true;
+  };
+
+  //    Valid dir values include "up","down","left","right"
+  board.placeShip = function (row, col, length, dir) {
+    if (this.checkSlots(row, col, length, dir)) {
+      const ship = newShip(length);
+      this.ships.push(ship);
+      const vect = dirVect(dir);
+      const init = new Vector(row, col);
+      for (let i = 0; i < length; i += 1) {
+        this.area[init.x0][init.x1].ship = ship;
+        init.add(vect);
+      }
+      return true;
+    }
+    return false;
+  };
+
+  board.receiveAttack = function (row, col) {
+    this.area[row][col].hit = true;
+    if (this.area[row][col].ship) {
+      this.area[row][col].ship.hit();
+    }
+  };
 
   return board;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { newBoard } from "./board";
 
 const myBoard = newBoard();
+myBoard.placeShip(0, 0, 3, "down");
+myBoard.placeShip(9, 9, 5, "left");
+myBoard.receiveAttack(9, 6);
+myBoard.receiveAttack(9, 7);
+myBoard.receiveAttack(9, 8);
+myBoard.receiveAttack(9, 9);
+myBoard.receiveAttack(9, 5);
+
 console.log(myBoard);
